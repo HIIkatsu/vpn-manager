3e1c021ef06df64d00675ce5ff429e826eb9fda7
HIIkatsu <259455136+HIIkatsu@users.noreply.github.com>
2026-05-19 14:45:34 +0300

    Add FastAPI app entrypoint and Telegram webhook core

---
+++ app/bot/core.py
@@ -0,0 +1,8 @@
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+
+from app.core.config import settings
+
+
+bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
+dp = Dispatcher()
